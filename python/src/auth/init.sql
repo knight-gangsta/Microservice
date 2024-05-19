b378c34891b8cce8425256d2fcd32d2fbbb1f253
guasonsad <guasonsad@GUASON-KING>
2024-05-19 17:57:34 -0500

    DockerConectado

--- python/src/auth/init.sql
+++ python/src/auth/init.sql
@@ -1,4 +1,17 @@
+-- Crear usuario para la base de datos
 CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Aauth123';
+
+-- Crear base de datos auth
+CREATE DATABASE auth;
+
+-- Otorgar todos los privilegios al usuario auth_user en la base de datos auth
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+-- Usar la base de datos auth
+USE auth;
+
+-- Crear tabla user
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'Auth123';
 
 CREATE DATABASE auth;
 
@@ -7,9 +20,11 @@ GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
 USE auth;
 
 CREATE TABLE user (
-    id INT NOT NULL AUTO_INCRIMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO user (email, password) VALUES ('nicolas@email.com','Admin123');
+INSERT INTO user (email, password) VALUES ('georgio@email.com', 'Admin123');
+
+
